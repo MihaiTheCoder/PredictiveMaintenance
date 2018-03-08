972a822df1112bf59c33db888e99c09f62bcc29d
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2018-03-08 11:48:58 +0200

    Add first regression algorithm

--- step_2/train_evaluate_regression_models.py
+++ step_2/train_evaluate_regression_models.py
@@ -8,20 +8,30 @@ def get_top_features(dataset, n_features):
     train_correlation = abs(train_dataset.corrwith(rul_column))
     important_features = train_correlation.sort_values(ascending=False)[:n_features]
 
-    dataset_top_features = dataset[list(important_features.index)]
-    return dataset_top_features
+    return list(important_features.index)
 
 
 if __name__ == '__main__':
-    df = pd.read_csv("../process_input/train_dataset_1_of_3.csv")
+    df_train = pd.read_csv("../process_input/train_dataset_1_of_3.csv")
+    test_df = pd.read_csv("../process_input/test_dataset_1_of_3.csv")
 
     #  Not useful for regression models
-    df.drop(labels=['label1', 'label2'], axis=1, inplace=True)
+    df_train.drop(labels=['label1', 'label2'], axis=1, inplace=True)
 
+    top_features = get_top_features(df_train, 35)
     #  get top 35 features (35 chosen at random)
-    df_top_features = get_top_features(df, 35)
+    df_train_top_features = df_train[top_features]
 
     regressor = GradientBoostingRegressor(n_estimators=100, min_samples_leaf=10, learning_rate=0.2, max_leaf_nodes=20)
-    est = regressor.fit(df_top_features.values, df['RUL'].values)
+    est = regressor.fit(df_train_top_features.values, df_train['RUL'].values)
 
-    print(df_top_features)
+    # features to use for prediction
+    test_df_features = test_df[top_features]
+
+    predictions = est.predict(test_df_features.values)
+
+    predictions_truth = pd.DataFrame({'Prediction': pd.Series(predictions), 'RUL': pd.Series(test_df['RUL'].values)})
+    print(predictions_truth)
+
+
+    #print(predictions_truth)
