d76788f252a69c1add1929a78d7f67c11963c073
Andrei.Nemes <andrei.nemes@qubiz.com>
2018-03-12 15:16:46 +0200

    Add Evaluation model

--- step_2/train_evaluate_binary_classification.py
+++ step_2/train_evaluate_binary_classification.py
@@ -3,6 +3,7 @@ from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import roc_auc_score
 
 def get_top_features(dataset, n_features):
     label_column = dataset['label1']
@@ -43,8 +44,50 @@ def get_binary_classification_predictions(training_features_df, training_label_v
     return predictions_truth
 
 
+def get_beamer_table_values(truth, prediction):
+    true_positive = 0
+    false_positive = 0
+    true_negative = 0
+    false_negative = 0
+
+    for i in range(0, len(truth)):
+        if truth[i] == prediction[i] and truth[i] == 1:
+            true_positive = true_positive + 1
+        if truth[i] == prediction[i] and truth[i] == 0:
+            true_negative = true_negative + 1
+        if truth[i] != prediction[i] and truth[i] == 1:
+            false_negative = false_negative + 1
+        if truth[i] != prediction[i] and truth[i] == 0:
+            false_positive = false_positive + 1
+
+
+    return true_positive, false_negative, false_positive, true_negative
+
+
 def evaluate_bianry_classification_models(predictions_truth):
-    return predictions_truth
+    prediction_columns = list(predictions_truth.columns.values)
+    prediction_columns.remove('label')
+    truth = predictions_truth['label'].values
+    list_of_features = list()
+    for prediction_column in prediction_columns:
+        prediction = predictions_truth[prediction_column].values
+        positive_label = 1
+        negative_label = 0
+        true_positive, false_negative, false_positive, true_negative = get_beamer_table_values(truth, prediction)
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)
+        f_score = 2 * (precision * recall) / (precision + recall)
+        auc_score = roc_auc_score(truth, prediction)
+        list_of_features.append([true_positive, false_negative, false_positive, true_negative,
+                                 positive_label, negative_label, precision, recall, accuracy,
+                                 f_score, auc_score])
+
+    return pd.DataFrame(list_of_features, columns=['True Positive', 'False Negative',
+                                                   'False Positive', 'True Negative',
+                                                   'Positive Label', 'Negative Label',
+                                                   'Precision', 'Recall', 'Accuracy',
+                                                   'F1 Score', 'AUC'])
 
 
 if __name__ == '__main__':
