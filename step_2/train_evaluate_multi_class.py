1954c4c6cfedaefb3e54788fd40f5837ecca0b53
Ioan MORAR <ioan.morar@qubiz.com>
2018-03-14 10:38:47 +0200

    Add ordinal regression

--- step_2/train_evaluate_multi_class.py
+++ step_2/train_evaluate_multi_class.py
@@ -5,6 +5,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from sklearn.externals import joblib
+from mord import LogisticIT, LAD, LogisticSE
+from math import sqrt
 
 
 def get_top_features(dataset, n_features):
@@ -41,10 +43,22 @@ def get_multiclass_classification_predictions(training_features_df, training_lab
     joblib.dump(mlp_classifier, 'multiclass/NeuralNetworkMLPClassifier.pkl')
     mlp_predicted = mlp_classifier.predict(testing_features_df)
 
+
+    nn_ordinal_regression = LogisticIT()
+    nn_ordinal_regression.fit(training_features_df, training_label_values)
+    nn_ordinal_regression_predicted = nn_ordinal_regression.predict(testing_features_df)
+
+    lr_ordinal_regression = LogisticSE()
+    lr_ordinal_regression.fit(training_features_df, training_label_values)
+    lr_ordinal_regression_predicted = lr_ordinal_regression.predict(testing_features_df)
+
+
     predictions_truth = pd.DataFrame({'LogisticRegression_Prediction': pd.Series(logreg_predicted),
                                       'AdaBoostDecisionTreeClassifier_Prediction': pd.Series(ada_classifier_predicted),
                                       'NeuralNetworkMLPClassifier_Prediction': pd.Series(mlp_predicted),
                                       'RandomForest_Prediction': pd.Series(random_forest_predicted),
+                                      'Ordinal Regression NN': pd.Series(nn_ordinal_regression_predicted),
+                                      'Ordinal Regression LR': pd.Series(lr_ordinal_regression_predicted),
                                       'label': pd.Series(testing_label_values)})
 
     return predictions_truth
@@ -65,20 +79,32 @@ def evaluate_multiclass_classification_models(predictions_truth):
 
         #accuracy
         overall_accuracy = metrics.accuracy_score(y_true=truth, y_pred=prediction)
-        avg_accuracy = 0
+        sum_avg = 0
+        for k in range(0, len(mat)):
+            S = mat[k][k] / mat.sum(axis=1)[k]
+            sum_avg = sum_avg + S
+        avg_accuracy = sum_avg / len(mat)
+
         #precision
         micro_precision = metrics.precision_score(y_true=truth, y_pred=prediction, average='micro')
         macro_precision = metrics.precision_score(y_true=truth, y_pred=prediction, average='macro')
+
         #recall
         micro_recall = metrics.recall_score(y_true=truth, y_pred=prediction, average='micro')
         macro_recall = metrics.recall_score(y_true=truth, y_pred=prediction, average='macro')
 
+        mean_zero_one_error = metrics.zero_one_loss(truth, prediction)
+        mean_absolute_error = metrics.mean_absolute_error(truth, prediction)
+        root_mean_squared_error = sqrt(metrics.mean_squared_error(truth, prediction))
+
+
         list_of_features.append([overall_accuracy, avg_accuracy, micro_precision, macro_precision,
-                                 micro_recall, macro_recall])
+                                 micro_recall, macro_recall, mean_zero_one_error,mean_absolute_error,root_mean_squared_error])
 
     return pd.DataFrame(list_of_features, columns=['Overall Accuracy', 'Averaged Accuracy',
                                                    'Micro-averaged Precision', 'Macro-averaged Precision',
-                                                   'Micro-averaged Recall', 'Macro-averaged Recall'],
+                                                   'Micro-averaged Recall', 'Macro-averaged Recall',
+                                                   'Mean Zero One Error', 'Mean Absolute Error', 'Root Mean Squared Error'],
                                                     index=prediction_columns)
 
 
